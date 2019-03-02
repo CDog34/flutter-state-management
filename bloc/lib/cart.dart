0fe44d8d5b8b661218e72976202589f0275e245f
caojunquan <caojunquan@bilibili.com>
2019-03-03 00:28:53 +0800

    + Use Bloc

--- bloc/lib/cart.dart
+++ bloc/lib/cart.dart
@@ -1,4 +1,7 @@
+import 'package:base/bloc.dart';
+import 'package:base/cart_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartPage extends StatelessWidget {
   @override
@@ -8,10 +11,15 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Cart"),
       ),
-      body: ListView.builder(
-        itemBuilder: (ctx, idx) => ListTile(
-              title: Text("商品 $idx"),
-              onTap: () => print(idx.toString()),
+      body: BlocBuilder(
+        bloc: BlocProvider.of<CartBloc>(context),
+        builder: (ctx, Cart cart) => ListView.builder(
+              itemCount: cart.count,
+              itemBuilder: (ctx, idx) => ListTile(
+                    title: Text("商品 ${cart.list[idx]}"),
+                    onTap: () => BlocProvider.of<CartBloc>(ctx)
+                        .dispatch(CartEvent.remove(cart.list[idx])),
+                  ),
             ),
       ),
     );
